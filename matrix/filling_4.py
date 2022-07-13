029ff62747e143d97896f5e54e0248d53600a67f
Ilya <ilya@kostech.ru>
2022-07-13 12:12:13 +0300

    Заполнение 4

---
+++ matrix/filling_4.py
@@ -0,0 +1,10 @@
+n = int(input())
+matrix = [[1]*n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if i > j and i < n - 1 - j or i < j and i > n-1-j:
+            matrix[i][j] = 0
+for r in range(n):
+    for c in range(n):
+        print(matrix[r][c], end=' ')
+    print()
