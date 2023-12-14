dcd49f293ef08788e7cf81bac28c7cee540c8937
Ilya <ilya@kostech.ru>
2023-12-14 20:36:17 +0300

    Рекурсивная функция

--- function_and_stack.py
+++ function_and_stack.py
@@ -1,7 +1,20 @@
-def s(a, *vs, b=10):
-   res = a + b
-   for v in vs:
-       res += v
-   return res
+# def s(a, *vs, b=10):
+#    res = a + b
+#    for v in vs:
+#        res += v
+#    return res
+#
+# print(s(21))
 
-print(s(21))
+a, b = map(int, input().split())
+
+def C(n, k):
+    if k == 0:
+        return 1
+    elif k > n:
+        return 0
+    else:
+        return C(n-1, k) + C(n-1, k-1)
+
+y = C(a, b)
+print(y)
